a39fbbd76e07da1883ba288591fffaddfc6ad73c
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-08 21:39:02 -0700

    Task 4__Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing
+ * 
+ * Using puts instead of printf
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
