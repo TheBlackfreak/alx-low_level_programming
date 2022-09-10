f4933df6cff74416a5d03fd94ab47bf1742e36f1
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-09 17:37:54 -0700

    Task 6__Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description- using main function
+ *
+ * This program prints numbers from 0-9
+ */
+int main(void)
+{
+	int n;
+	/*n is an acronym for number*/
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+}
+	putchar('\n');
+return (0);
+}
