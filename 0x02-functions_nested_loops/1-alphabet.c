2063578ead966de1596a9d5d5b72350ca910db9c
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-13 16:18:16 -0700

    Task 1__Insomia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet- entry point
+ *
+ * Description- This program prints the alphabet in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
