36e0441dfac86cf6ec0f191d47f6a1fd180de632
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-13 16:33:48 -0700

    Task 2__Alphabet *correction*

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,14 @@
 /**
  * print_alphabet_x10- entry point
  *
- * Description- This program prints 10 times the alphabet, in lowercase, followed by a new line.
+ * Description- This program prints 10 times the alphabet, in lowercase.
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char c, i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
