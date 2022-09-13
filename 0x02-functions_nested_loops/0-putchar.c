0b6592b79b2c783a6035f30bc3387542bd6890a7
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-13 13:21:08 -0700

    Task 0__Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Description- A program that prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+return (0);
+}
