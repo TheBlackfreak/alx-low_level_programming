ae97601f14c44377dd60a69e4cc166cc69e8fc09
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-13 17:55:04 -0700

    Task 6__ABS

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio>
+/**
+ * _abs- entry point
+ *
+ * Description: This program computes the absolute value of an integer
+ *
+ * @c:  is the int that will use for the argument of the function
+ *
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+		return (c * -1);
+}
