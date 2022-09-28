1349e5002db09cc41a04e7d686b15efdb7e55ced
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-27 20:26:11 -0700

    Task 2 & 3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,13 +4,12 @@
  * _strchr - locates a character in a string
  *
  * @s: string to check
- *
  * @c: character to check for
  *
  * Return: pointer to spot in s with c or null
  */
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
 	int i;
 
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,15 @@
 #include "main.h"
+
 /**
  * _strspn - gets length of a prefix substring
  *
  * @s: string to check
- *
  * @accept: string to check against
  *
- * Return: number to bytes of s in accept
+ * Return: number of bytes of s in accept
  */
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
