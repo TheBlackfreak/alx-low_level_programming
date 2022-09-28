8b7bf2d55eee27f05ed92ac382f31cf61fc89356
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-09-27 20:16:42 -0700

    Task 1__3rd trial

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,16 @@
 #include "main.h"
+
 /**
  * _memcpy - copies a memory area
  *
  * @dest: memory area to be copied to
- *
- * @src: memory area to be copied to
- *
+ * @src: memory area to be copied from
  * @n: number of bytes to be copied
  *
  * Return: pointer to the copied memory block
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
