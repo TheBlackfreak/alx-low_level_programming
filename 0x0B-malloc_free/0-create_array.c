b303bd0851c73d9773e6cfbde8fd1fb9c68c85fd
TheBlackfreak <okwuikeonyekanoel@gmail.com>
2022-10-04 16:31:06 -0700

    Added main.c file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
  * points to array if everthing is normal
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int index;
